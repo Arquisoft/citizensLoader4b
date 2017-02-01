88f36fb688b24c417fc945eb33185705ffddb06b
AdriánGarcíaLumbreras <adrianglumbreras@gmail.com>
2017-02-01 16:39:03 +0100

    Creada la clase Citizen

---
+++ src/main/java/es/uniovi/asw/model/Citizen.java
@@ -0,0 +1,119 @@
+package es.uniovi.asw.model;
+
+import java.sql.Date;
+
+public class Citizen {
+
+	private long id;
+
+	private String nombre;
+	private String apellidos;
+	private String email;
+	private Date fechaNacimiento;
+	private String residencia;
+	private String nacionalidad;
+	private String dni;
+
+	private String nombreUsuario;
+	private String password;
+
+	public Citizen() {
+
+	}
+
+	public Citizen(long id, String nombre, String apellidos, String email, Date fechaNacimiento, String residencia,
+			String nacionalidad, String dni, String nombreUsuario, String password) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.email = email;
+		this.fechaNacimiento = fechaNacimiento;
+		this.residencia = residencia;
+		this.nacionalidad = nacionalidad;
+		this.dni = dni;
+		this.nombreUsuario = nombreUsuario;
+		this.password = password;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getResidencia() {
+		return residencia;
+	}
+
+	public void setResidencia(String residencia) {
+		this.residencia = residencia;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
