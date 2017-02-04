7e229d3fb777cf07299f136a48228f14d8777f99
Raúl Gómez Pérez <raul.infotek@gmail.com>
2017-02-04 21:04:50 +0100

    Eliminada implementación de interfaz JPA (no es necesaria)

--- src/main/java/es/uniovi/asw/persistence/JpaCitizen.java
+++ src/main/java/es/uniovi/asw/persistence/JpaCitizen.java
@@ -1,11 +1,9 @@
 package es.uniovi.asw.persistence;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import es.uniovi.asw.model.Citizen;
 
 public interface JpaCitizen extends JpaRepository<Citizen, Long> {
 
-	public List<Citizen> save(List<Citizen> citizens);
 
 }
