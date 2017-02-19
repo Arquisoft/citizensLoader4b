556f3926a18eae0696e0d9d93f7178f464a40d2c
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2017-02-19 17:52:09 +0100

    Revisando RCitizens 2

--- src/test/java/es/uniovi/asw/parser/RCitizensTest.java
+++ src/test/java/es/uniovi/asw/parser/RCitizensTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import es.uniovi.asw.common.CitizenException;
@@ -12,6 +13,16 @@ import es.uniovi.asw.parser.RCitizens;
 import es.uniovi.asw.parser.ReadCitizens;
 
 public class RCitizensTest {
+	private ReadCitizens rs = new RCitizens();
+	private List<Citizen> citizens;
+	private Exception exception;
+
+	@Before
+	public void inicializarTest() {
+		citizens = null;
+		exception = null;
+
+	}
 
 	/**
 	 * Test que comprueba el correcto funcionamiento de la clase RCitizens.
@@ -20,36 +31,43 @@ public class RCitizensTest {
 	 *             Excepción ocurrida durante la ejecución
 	 */
 	@Test
-	public void testRCitizens() throws CitizenException {
-		// 2a
-		ReadCitizens rs = new RCitizens();
-		List<Citizen> citizens = null;
-		Exception exception = null;
+	public void testRutaCorrecta() throws CitizenException {
 		try {
 			citizens = rs.readCitizens(
 					"..\\citizensLoader4b\\src\\test\\resources\\test.xlsx");
 		} catch (Exception e) {
 			exception = e;
 		}
-		assertNotEquals(citizens.size(), 0);
+		assertNotNull(citizens);
 		assertNull(exception);
+		assertNotEquals(citizens.size(), 0);
 
+	}
+
+	@Test
+	public void testRutaIncorrecta() throws CitizenException {
 		try {
 			citizens = rs.readCitizens(
 					"..\\citizensLoader4b\\src\\test\\resources\\tes.xlsx");
 		} catch (Exception e) {
 			exception = e;
 		}
+		assertNull(citizens);
 		assertNotNull(exception);
+		assertEquals(CitizenException.class, exception.getClass());
 		assertEquals("Fichero no encontrado", exception.getMessage());
+	}
 
+	@Test
+	public void testRutaVacia() throws CitizenException {
 		try {
 			citizens = rs.readCitizens("");
 		} catch (Exception e) {
 			exception = e;
 		}
+		assertNull(citizens);
 		assertNotNull(exception);
-		System.out.println(exception.getMessage());
+		assertEquals(CitizenException.class, exception.getClass());
 		assertEquals("Error en el fichero la extensión del archivo",
 				exception.getMessage());
 	}
